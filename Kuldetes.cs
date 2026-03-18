d5474df095056870f88db61327554d5228368452
qgedeak@gmail.com <qgedeak@gmail.com>
2026-03-18 10:26:45 +0100

    Add project files.

---
+++ Kuldetes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NASACLII
+{
+    public class Kuldetes
+    {
+        public string Nev {  get; private set; }
+        public int Ev { get; private set; }
+        public string Celpont { get; private set; }
+        public int Legenyseg { get; private set; }
+        public bool Sikeres { get; private set; }
+        public string Leiras { get; private set; }
+        public double Koltseg { get; private set; }
+        public double HasznosTeher { get; private set; }
+
+        public Kuldetes(string sor)
+        {
+            Nev = sor.Split(';')[0];
+            Ev = int.Parse(sor.Split(';')[1]);
+            Celpont = sor.Split(';')[2];
+            Legenyseg = int.Parse(sor.Split(';')[3]);
+            Sikeres = sor.Split(';')[4] == "Igen" ? true : false;
+            Leiras = sor.Split(';')[5];
+            Koltseg = double.Parse(sor.Split(';')[6]);
+            HasznosTeher = double.Parse(sor.Split(';')[7]);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace NASACLII
+{
+    public class Program
+    {
+        static List<Kuldetes> kuldetesLista = new List<Kuldetes>();
+        static void Main(string[] args)
+        {
+
+            string[] beolvasott = File.ReadAllLines("NASAmissions.txt");
+            foreach (var item in beolvasott.Skip(1))
+            {
+                kuldetesLista.Add(new Kuldetes(item));
+            }
+            hanyKuldetes();
+
+            //4.feladat
+            while (true)
+            {
+                Console.Write("Adja meg egy küldetés nevének részletét: ");
+                string bekertNev = Console.ReadLine();
+                bool talalt = false;
+                for (int i = kuldetesLista.Count()-1; i >= 0; i--)
+                {
+                    if (kuldetesLista[i].Nev.ToLower().Contains(bekertNev.ToLower()))
+                    {
+                        Console.WriteLine($"Talált küldetés: {kuldetesLista[i].Nev}, {kuldetesLista[i].Ev}, {kuldetesLista[i].Celpont}, {(kuldetesLista[i].Sikeres ? "Sikeres" : "Sikertelen")}");
+                        talalt = true;
+                        break;
+                    }                    
+                }
+                if (talalt==true)
+                {
+                    break;
+                }
+            }
+        }
+        //3. feladat
+        public static void hanyKuldetes()
+        {
+            Console.WriteLine($"3. feladat: {kuldetesLista.Count} küldetés található az állományban.");
+        }
+    }
+}
